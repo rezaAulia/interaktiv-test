a829a029ea419d51705a479aa3ef25a7a3dce932
reza-aulia <reza.aulia.1994@gmail.com>
2015-11-10 12:06:50 +0700

    clear HomeController. and make new function to index page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,5 +1,8 @@
 <?php namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Sample;
+
 class HomeController extends Controller {
 
 	/*
@@ -13,15 +16,7 @@ class HomeController extends Controller {
 	|
 	*/
 
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
+	
 
 	/**
 	 * Show the application dashboard to the user.
@@ -30,7 +25,8 @@ class HomeController extends Controller {
 	 */
 	public function index()
 	{
-		return view('home');
+		return view('index');
 	}
+
 
 }
